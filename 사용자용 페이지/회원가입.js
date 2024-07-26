dddf757f06c0e35b1cadb14506d5853966542b5d
조민정 <mg518220@gmail.com>
2024-07-26 17:25:15 +0900

    사용자용 메인페이지 완성(북플릭스 제외)

--- 사용자용 페이지/회원가입.js
+++ 사용자용 페이지/회원가입.js
@@ -9,17 +9,31 @@ document.getElementById("check-username").addEventListener("click", function() {
 });
 
 document.getElementById("gaip_1").addEventListener("click", function() {
-    const name = document.getElementById("name").value;
+    const name = document.getElementById("name_").value;
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
     const phone = document.getElementById("phone").value;
     const emailUser = document.getElementById("email-user").value;
     const emailDomain = document.getElementById("email-domain").value;
+    const addressnum = document.getElementById("addressnum").value;
+    const address = document.getElementById("address").value;
+    const addressinfo = document.getElementById("addressinfo").value;
 
+    console.log("이름 :" + name)
+    console.log("아이디 :" + username)
+    console.log("비번 :" + password)
+    console.log("비번 확인 :" + confirmPassword)
+    console.log("전화번호 :" + phone)
+    console.log("우편주소 :" + addressnum)
+    console.log("기본주소 :" + address)
+    console.log("상세주소 :" + addressinfo)
+    console.log("이메일 :" + emailUser)
+    console.log("이메일 도매인 :" + emailDomain)
     
 
-    if (!name || !username || !password || !confirmPassword || !phone || !emailUser || !emailDomain) {
+    if (!name || !username || !password || !confirmPassword || !phone || !emailUser || !emailDomain 
+        || !addressnum || !address || !addressinfo) {
         alert("모든 필드를 입력하세요.");
         return;
     }
@@ -30,31 +44,31 @@ document.getElementById("gaip_1").addEventListener("click", function() {
     }
 
     alert("회원가입이 완료되었습니다.");
+    window.location.href="01.main_user_login.html";
 });
 
 function msg(){
 
     let idmsg = document.querySelector("#username-message");
-    let pw = document.getElementById("password");
-    let username = document.getElementById("username").value;
-
+    let pw = document.querySelector("#password");
+    
     pw.addEventListener("focus", function(){
-        console.log("비번")
+        let username = document.querySelector("#username").value;
+        // console.log("아이디 : " + username)
         if(username == ""){
-            console.log("dkflksdf")
+            console.log(username);
             idmsg.style.display="block";
         }
     })
     
 
     let conpw = document.getElementById("confirm-password");
-    let password = document.getElementById("password").value;
     let pwmsg = document.querySelector("#password-message");
-
+    
     conpw.addEventListener("focus", function(){
-        console.log("비번")
+        let password = document.getElementById("password").value;
+        // console.log("비번 : " + password)
         if(password == ""){
-            console.log("dkflksdf")
             pwmsg.style.display="block";
         }
     })
