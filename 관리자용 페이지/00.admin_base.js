9a12e9a4ed1a6d506a1609d20e07eabaad48c5a6
박규태 <qsz3737@naver.com>
2024-07-29 11:08:16 +0900

    관리자용페이지 logo

--- 관리자용 페이지/00.admin_base.js
+++ 관리자용 페이지/00.admin_base.js
@@ -6,33 +6,39 @@ let i3 = document.querySelector("#i3");
 let i4 = document.querySelector("#i4");
 let i5 = document.querySelector("#i5");
 let i6 = document.querySelector("#i6");
+let logo = document.querySelector('#logo');
 
 let login = document.querySelector("#login");
 let userin = document.querySelector("#userin");
 
 function pageMove(){
-    i1.addEventListener('click', function(){
-        window.location.href="두정도서관 정보.html"
-    })
-    i2.addEventListener('click', function(){
-        window.location.href="best.html"
-    })
-    i3.addEventListener('click', function(){
-        window.location.href=""
-    })
-    i4.addEventListener('click', function(){
-        window.location.href=""
-    })
-    i5.addEventListener('click', function(){
-        window.location.href="02.mypage_use.html"
-    })
+    // i1.addEventListener('click', function(){
+    //     window.location.href="두정도서관 정보.html"
+    // })
+    // i2.addEventListener('click', function(){
+    //     window.location.href="best.html"
+    // })
+    // i3.addEventListener('click', function(){
+    //     window.location.href=""
+    // })
+    // i4.addEventListener('click', function(){
+    //     window.location.href=""
+    // })
+    // i5.addEventListener('click', function(){
+    //     window.location.href="02.mypage_use.html"
+    // })
 
-    login.addEventListener('click', function(){
-        window.location.href="kim/로그인.html"
-    })
-    userin.addEventListener('click', function(){
-        window.location.href="회원가입.html"
+    // login.addEventListener('click', function(){
+    //     window.location.href="kim/로그인.html"
+    // })
+    // userin.addEventListener('click', function(){
+    //     window.location.href="회원가입.html"
+    // })
+    logo.addEventListener('click', function(){
+        console.log('클릭')
+        window.location.href = "admin_main.html"
     })
 }
+
 
 pageMove();
