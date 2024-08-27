e8778be1646b3bcb80439a8f6ec2e9338596669d
박규태 <qsz3737@naver.com>
2024-08-27 16:14:18 +0900

    mes_page 합치기~

---
+++ mmes_page/src/main/java/mes_controller/HumanController.java
@@ -0,0 +1,25 @@
+package mes_controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HumanController
+ */
+@WebServlet("/human")
+public class HumanController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("MES_직원관리 doGet 실행!");
+		request.setCharacterEncoding("utf-8");
+	    response.setContentType("text/html; charset=utf-8;");
+		
+		request.getRequestDispatcher("/WEB-INF/mes/mes_human/human.jsp").forward(request, response);
+	}
+
+}
