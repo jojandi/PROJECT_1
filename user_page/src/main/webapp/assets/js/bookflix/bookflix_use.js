b3296f6979d7f9912b5aab6e4c1e75dc59f15213
조민정 <mg518220@gmail.com>
2024-08-28 16:32:07 +0900

    북플릭스 리뷰 기능 추가 ing..

--- user_page/src/main/webapp/assets/js/bookflix/bookflix_use.js
+++ user_page/src/main/webapp/assets/js/bookflix/bookflix_use.js
@@ -1,9 +1,32 @@
-let bnt = document.querySelector("#fixed-button");
+function popupOn(){
+    let popup = document.querySelectorAll(".popup");
+    let popupBnt = document.querySelectorAll(".popupBnt");
+    let end = document.querySelectorAll(".popup-close");
 
-function subOn(){
-	bnt.addEventListener('click', function(){
-    	alert('구독되었습니다!');
-	})
+    for(let i = 0; i < popup.length; i++){
+        popupBnt[i].addEventListener('click', function(){
+            popup[i].style.display='block';
+        })
+        end[i].addEventListener('click', function(){
+            popup[i].style.display='none';
+        })
+    }
 }
 
-subOn();
+function reviewOn(){
+    let review = document.querySelector("#reviewPopup");
+    let reviewBnt = document.querySelector("#reviewBnt");
+    let end = document.querySelector("#end");
+
+    reviewBnt.addEventListener('click', function(){
+        review.style.display='block';
+    })
+    end.addEventListener('click', function(){
+        alert("리뷰 작성이 완료되었습니다. ");
+        review.style.display='none';
+    })
+    
+}
+
+popupOn();
+reviewOn();
