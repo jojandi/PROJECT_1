b50ea7e71e9713ece6789366b5649d52b53ae154
제섭test <>
2024-09-08 19:25:02 +0900

    전부완료 ㅎㅎ 수고연

--- mmes_page/src/main/java/mes_service/MesMainService.java
+++ mmes_page/src/main/java/mes_service/MesMainService.java
@@ -14,7 +14,7 @@ public class MesMainService {
     }
 
     // 월별 출고 통계 데이터를 가져오는 메서드
-    public List<MesMainDTO> getStatisticsByGenre(Integer year, Integer month) {
+    public List<MesMainDTO> getStatisticsByGenre(int year, int month) {
         // 연도와 월에 맞는 장르별 출고 데이터를 가져오는 DAO 메서드 호출
         return mesMainDAO.getStatisticsByGenre(year, month);
     }
